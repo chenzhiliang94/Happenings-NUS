fcf2aa1942b22f32bd6ec5953e30aef53d9c9d28
chenzhiliang94 <chenzhiliang94@gmail.com>
2017-05-10 18:08:19 +0800

    Add new Maven project to repo

---
+++ Happenings-NUS/happenings-NUS/src/main/java/happenings_NUS/happenings_NUS/App.java
@@ -0,0 +1,13 @@
+package happenings_NUS.happenings_NUS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
